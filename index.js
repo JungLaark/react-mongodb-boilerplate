eef915ac66cc276651763b08a2dad0187cb18875
Chan Jung <chenone839@gmail.com>
2021-08-02 15:26:12 +0900

    register

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 //시작점
 const express = require('express');
 const mongoose = require('mongoose');
-
+const bodyParser = require('body-parser');
+//express 호출
 const app = express();
 const port = 5000;
+
+//model 가져옴
+const {User} = require('./models/User');
+
+//body parser 옵션주기
+///application/x-www-form-urlencoded 데이터를 분석해서 가지고 올 수 있게함 
+app.use(bodyParser.urlencoded({extends: true}));
+///application/json 형식 데이터 
+app.use(bodyParser.json());
 
 mongoose.connect('mongodb+srv://jcs:jcs@makeyoutube.0uoyl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
     useNewUrlParser: true,
@@ -13,10 +23,28 @@ mongoose.connect('mongodb+srv://jcs:jcs@makeyoutube.0uoyl.mongodb.net/myFirstDat
 }).then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err));
 
-  
 
+//routes
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.send('Hello World ggg');
+});
+
+//회원가입
+app.post('/register', (req, res) => {
+    //아이디 비번 등 가져올 것임
+    //db에 저장 할 것임 
+    const user = new User(req.body);
+
+    //db 저장 
+    user.save((err, userInfo) => {
+
+        if(err){
+            return res.json({success: false, err });
+        }
+
+        return res.status(200).json({success: true});
+    });
+
 });
 
 app.listen(port, () => {
--- readme.md
+++ readme.md
@@ -29,9 +29,38 @@
 
     2. start the ssh-agent in the background
         eval "$(ssh-agent -s)"
-
+    '''BASH
     git remote add origin https://github.com/JungLaark/react-mongodb-boilerplate.git
     git branch -M main
     git push -u origin main
 
-## 회원가입 기능 만들기 
+    git add .
+    git commit -m "comment"
+    git push -u origin main
+    ```
+
+## 회원가입 기능 만들기  
+### body-parser : 데이터를 분석(parse)해서 req.body 로 출력해주는 것 
+    - npm install body-parser --save
+### register router 코드 추가 
+### postman 사용 
+    0. post localhost:5000/register
+    1. body 탭
+    2. raw 선택
+    3. json 선택 
+    ```json
+    4. {
+        "name" : "Jung",
+        "email" : "che@naver.com",
+        "password" : "123345"
+         }
+    ```
+         이거 넣어줌 
+
+## node mon 다운로드 
+### 소스를 변경할 때 그걸 감지해서 자동으로 서버에 반영시켜주는 툴 
+    -  변경 후 refresh 해도 바뀌지 않는 것을 자동으로 변경되게끔 해줌 
+    - npm install nodemon --save-dev
+    - package.json 에 추가 
+      - "backend": "nodemon index.js",
+
